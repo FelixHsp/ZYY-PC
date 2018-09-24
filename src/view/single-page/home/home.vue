d01232356b3db863de9409ed30db6dd1f5ce39dd
Felix <13115412392@163.com>
2018-09-24 14:36:43 +0800

    0

---
+++ src/view/single-page/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+      <img class="index-img" :src="index" alt="">
+</template>
+
+<script>
+import index from '@/assets/images/index.jpg'
+export default {
+  name: 'home',
+  data () {
+    return {
+      index
+    }
+  }
+}
+</script>
+
+<style>
+.index-img{
+    height: 100%;
+    width: 100%;
+}
+</style>
