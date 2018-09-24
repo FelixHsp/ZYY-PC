c968ca08c58e600f39aa13b4cc38543972bfcbf6
Felix <13115412392@163.com>
2018-09-24 08:48:02 +0800

    begin

---
+++ src/router/routers.js
@@ -0,0 +1,196 @@
+import Main from '@/view/main'
+import parentView from '@/components/parent-view'
+
+/**
+ * iview-admin中meta除了原生参数外可配置的参数:
+ * meta: {
+ *  hideInMenu: (false) 设为true后在左侧菜单不会显示该页面选项
+ *  notCache: (false) 设为true后页面不会缓存
+ *  access: (null) 可访问该页面的权限数组，当前路由设置的权限会影响子路由
+ *  icon: (-) 该页面在左侧菜单、面包屑和标签导航处显示的图标，如果是自定义图标，需要在图标名称前加下划线'_'
+ * }
+ */
+
+export default [
+  {
+    path: '/login',
+    name: 'login',
+    meta: {
+      title: 'Login - 登录',
+      hideInMenu: true
+    },
+    component: () => import('@/view/login/login.vue')
+  },
+  {
+    path: '/',
+    name: '_home',
+    redirect: '/home',
+    component: Main,
+    meta: {
+      hideInMenu: true,
+      notCache: true
+    },
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        meta: {
+          hideInMenu: true,
+          title: '首页',
+          notCache: true
+        },
+        component: () => import('@/view/single-page/home')
+      }
+    ]
+  },
+  {
+    path: '/components',
+    name: 'components',
+    meta: {
+      icon: 'logo-buffer',
+      title: '医生列表'
+    },
+    component: Main,
+    children: [
+      {
+        path: 'count_to_page',
+        name: 'count_to_page',
+        meta: {
+          icon: 'md-trending-up',
+          title: '医生详情页'
+        },
+        component: () => import('@/view/components/count-to/count-to.vue')
+      },
+      {
+        path: 'drag_list_page',
+        name: 'drag_list_page',
+        meta: {
+          icon: 'ios-infinite',
+          title: '添加医生'
+        },
+        component: () => import('@/view/components/drag-list/drag-list.vue')
+      }
+    ]
+  },
+  {
+    path: '/tools_methods',
+    name: 'tools_methods',
+    meta: {
+      hide: true
+    },
+    component: Main,
+    children: [
+      {
+        path: 'tools_methods_page',
+        name: 'tools_methods_page',
+        meta: {
+          icon: 'ios-hammer',
+          title: '用户列表'
+        },
+        component: () => import('@/view/tools-methods/tools-methods.vue')
+      }
+    ]
+  },
+  {
+    path: '/directive',
+    name: 'directive',
+    meta: {
+      hide: true
+    },
+    component: Main,
+    children: [
+      {
+        path: 'directive_page',
+        name: 'directive_page',
+        meta: {
+          icon: 'ios-navigate',
+          title: '就医指南'
+        },
+        component: () => import('@/view/directive/directive.vue')
+      }
+    ]
+  },
+  {
+    path: '/update',
+    name: 'update',
+    meta: {
+      icon: 'md-cloud-upload',
+      title: '病种分类'
+    },
+    component: Main,
+    children: [
+      {
+        path: 'update_table_page',
+        name: 'update_table_page',
+        meta: {
+          icon: 'ios-document',
+          title: '病种详情'
+        },
+        component: () => import('@/view/update/update-table.vue')
+      },
+      {
+        path: 'update_paste_page',
+        name: 'update_paste_page',
+        meta: {
+          icon: 'md-clipboard',
+          title: '添加病种'
+        },
+        component: () => import('@/view/update/update-paste.vue')
+      }
+    ]
+  },
+  {
+    path: '/argu',
+    name: 'argu',
+    meta: {
+      hideInMenu: true
+    },
+    component: Main,
+    children: [
+      {
+        path: 'params/:id',
+        name: 'params',
+        meta: {
+          icon: 'md-flower',
+          title: '动态路由',
+          notCache: true
+        },
+        component: () => import('@/view/argu-page/params.vue')
+      },
+      {
+        path: 'query',
+        name: 'query',
+        meta: {
+          icon: 'md-flower',
+          title: '带参路由',
+          notCache: true
+        },
+        component: () => import('@/view/argu-page/query.vue')
+      }
+    ]
+  },
+  {
+    path: '/401',
+    name: 'error_401',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/401.vue')
+  },
+  {
+    path: '/500',
+    name: 'error_500',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/500.vue')
+  },
+  {
+    path: '*',
+    name: 'error_404',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/404.vue')
+  }
+]
