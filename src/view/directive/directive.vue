66ca41cc80712906c9b287efab7873966d93a69f
Goodya <1799972130@qq.com>
2018-10-19 07:58:39 +0800

    洋

--- src/view/directive/directive.vue
+++ src/view/directive/directive.vue
@@ -1,14 +1,43 @@
 <template>
+  <div>
     <Row>
-        <Col span="22" offset="1" v-for="value in formInline" :key="value.index">
+        <Col span="22" offset="1" v-for=" (value,idx) in formInline" :key="idx">
             <Card dis-hover>
                 <p slot="title">{{value.article_title}}</p>
                 <p style="font-size: 12px;color: #000">文章作者：{{value.article_did}}  文章类型：{{value.article_class}}</p>
                 <p>{{value.article_content}}</p>
-                <p class="goDetail" style="color: #3399ff;font-size: 10px;float: right" @click="goDetail()">查看详情</p>
+                <p class="goDetail" style="color: #3399ff;font-size: 10px;float: right" @click="goDetail(idx)">查看详情</p>
             </Card>            
         </Col>
     </Row>
+    <card 
+      v-show="modal1"
+      title="文章详情页" id="card">
+      <div>
+        <i-button type="primary" @click="goback" id="back">
+            &lt;后退
+        </i-button>
+        <i-form :model="formItem" :label-width="80" method="post">
+          <Form-item label="文章标题">
+            <i-input :value.sync="formItem.article_title" placeholder="请输入"  name="title"></i-input>
+          </Form-item>
+          <Form-item label="文章作者">
+            <i-input :value.sync="formItem.article_did" name="did" :autosize="{minRows: 2,maxRows: 8}" placeholder="请输入..."></i-input>
+          </Form-item>
+          <Form-item label="文章类型">
+            <i-input :value.sync="formItem.article_class" name="class" :autosize="{minRows: 2,maxRows: 8}" placeholder="请输入..."></i-input>
+          </Form-item>
+          <Form-item label="文章详情">
+            <i-input :value.sync="formItem.article_content" type="textarea" name="content" :autosize="{minRows: 2,maxRows: 8}" placeholder="请输入..."></i-input>
+          </Form-item>          
+          <input type="text" :value.sync="formItem.aid" name="aid" v-show="false">
+          <input type="button" id="btnl" @click="info" value="更改">
+          <input type="button" id="btnr" @click="info1" value="删除">
+        </i-form>
+    </div>
+    </card>
+  </div>
+    
 </template>
 
 <script>
@@ -17,65 +46,79 @@ export default {
   name: "directive",
   data() {
     return {
-      formInline: []
+      formInline: [],
+      formItem: {
+        input: '',
+        select: '',
+        radio: 'male',
+        checkbox: [],
+        switch: true,
+        date: '',
+        time: '',
+        slider: [20, 50],
+        textarea: '',
+        aid:'',
+        did:'',
+        type:''
+      },
+      modal1:false,
     };
   },
   created() {
     this.send();
   },
   methods: {
-    ok() {
-      this.$Message.info("Clicked ok");
-      axios({
-        url: "http://localhost/zyy/user/article",
-        method: "post",
-        data: this.formInline,
-        transformRequest: function(obj) {
-          var str = [];
-          for (var p in obj) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          }
-          return str.join("&");
-        }
-      })
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    goDetail(idx) {
+      this.modal1=true;
+      this.formItem=this.formInline[idx]
+      console.log(this.formItem)
     },
-    cancel() {
-      this.$Message.info("Clicked cancel");
-      this.send();
-      axios({
-        url: "http://localhost/zyy/user/article",
-        method: "post",
-        data: this.correctdoc,
-        transformRequest: function(obj) {
-          var str = [];
-          for (var p in obj) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          }
-          return str.join("&");
-        }
-      })
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    goback (){
+      this.modal1=false;
+      console.log(this.formItem)
     },
-    goDetail() {
-      this.$router.push({
-        path: "page_list_page",
-        name: "page_list_page",
-        params: {
-          key: "key",
-          msgKey: this.msg
+    info () {
+      axios({
+        url: 'http://localhost/zyy/doctor/rearticle',
+        method: 'post',
+        data: this.formItem,
+        transformRequest: function (obj) {
+          var str = []
+          for (var p in obj) {
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+          }
+          return str.join('&')
         }
-      });
+      }).then(res => {
+        this.modal1=false;
+        alert('更改成功');
+        this.send();
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    info1 () {
+      
+      this.modal1=false;
+      axios({
+        url: 'http://localhost/zyy/doctor/delarticle',
+        method: 'post',
+        data: this.formItem,
+        transformRequest: function (obj) {
+          var str = []
+          for (var p in obj) {
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
+          }
+          return str.join('&')
+        }
+      }).then(res => {
+        alert('删除成功');
+        this.send();
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
     },
 
     send() {
@@ -94,5 +137,28 @@ export default {
 <style>
 .goDetail {
   cursor: pointer;
+}
+#card{
+  text-align: center;
+  width: 600px;
+  height: 400px;
+  position: absolute;
+  left: 400px;
+  top: 100px;
+}
+#btnl{
+  margin-left: 300px;
+  width: 50px;
+  height: 30px;
+}
+#btnr{
+  margin-left: 100px;
+  width: 50px;
+  height: 30px;
+}
+#back{
+  position: absolute;
+  left: 5px;
+  top: 10px;
 }
 </style>
